62d3ecfc93b674062ad387b01927d012c0f35ad8
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2025-09-02 18:46:45 +0530

    updates sdk version

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ export const TELEMETRY_SDK_PREFIX = "[TelemetrySDK]";
 export const HYPERLOOK_URL = "https://ingest.hyperlook.io/events/batch";
 
 // SDK version - automatically updated from package.json during build
-export const SDK_VERSION = "1.0.16";
+export const SDK_VERSION = "1.0.17";
 
 // Maximum batch size for telemetry events (100KB)
 export const MAX_BATCH_SIZE_BYTES = 100 * 1024;
